05c0b070d37e94bb4b4b8abb06a20082f06589df
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2025-06-04 15:32:50 +0300

    c3

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,15 @@ import { Immunity } from './components/immunity'
 import { Promo } from './components/promo'
 
 export function App() {
-
   return (
     <>
-      <h1 className="main-title">FitFuel</h1>
-      <Promo />
-      <Energy />
-      <Immunity />
-      <button className='button' style={
-        {
-          marginTop: '60px'
-        }
-      }>Оформить заказ(0)</button>
+      <main>
+        <h1 className="main-title">FitFuel</h1>
+        <Promo />
+        <Energy />
+        <Immunity />
+      </main>
+      <button className='button'>Оформить заказ(0)</button>
     </>
   )
 }

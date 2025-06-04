97c2bd9c2821f128951a75f2c4445ce96fae310f
PavlovIvan1 <pavlov-2011pavlov@yandex.ru>
2025-06-04 17:44:44 +0300

    c9

--- src/data/products.js
+++ src/data/products.js
@@ -11,34 +11,70 @@ export const products = {
       name: 'Кофеин',
       price: 699,
       image: './image1.png'
+    },
+    {
+      id: 3,
+      name: 'Женьшень',
+      price: 600,
+      image: './GENJEN600.jpg'
+    },
+    {
+      id: 4,
+      name: 'Витамин B12',
+      price: 600,
+      image: './B12-600.jpg'
     }
   ],
   focus: [
     {
-      id: 3,
-      name: 'Родиола',
-      price: 699,
-      image: './image2.png'
+      id: 5,
+      name: 'Ежовик',
+      price: 1000,
+      image: './EJOVIK1000.jpg'
     },
     {
-      id: 4,
-      name: 'Гинкго',
-      price: 699,
-      image: './image3.png'
+      id: 6,
+      name: 'L-карнитин',
+      price: 800,
+      image: './L-CARNITINE800.jpg'
+    },
+    {
+      id: 7,
+      name: 'Омега-3',
+      price: 550,
+      image: './OMEGA3-550.jpg'
+    },
+    {
+      id: 8,
+      name: 'Гинкго Билоба',
+      price: 1000,
+      image: './GINKOBIBOLA1000.jpg'
     }
   ],
   sport: [
     {
-      id: 5,
+      id: 9,
       name: 'Креатин',
-      price: 699,
-      image: './image4.png'
+      price: 1800,
+      image: './CREATINE1800.jpg'
     },
     {
-      id: 6,
+      id: 10,
       name: 'BCAA',
-      price: 699,
-      image: './image5.png'
+      price: 1200,
+      image: './BCAA1200.jpg'
+    },
+    {
+      id: 11,
+      name: 'Протеин',
+      price: 4500,
+      image: './PROTEIN4500.jpg'
+    },
+    {
+      id: 12,
+      name: 'ZMA',
+      price: 800,
+      image: './ZMA800.jpg'
     }
   ]
 }; 
